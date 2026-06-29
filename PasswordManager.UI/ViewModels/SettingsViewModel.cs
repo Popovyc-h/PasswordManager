760f5f712a04427663b67ec4773a13d407851d76
Ivan <popovichtanja86@gmail.com>
2026-06-29 16:43:57 +0300

    feat: added dynamic theme

--- PasswordManager.UI/ViewModels/SettingsViewModel.cs
+++ PasswordManager.UI/ViewModels/SettingsViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace PasswordManager.UI.ViewModels;
+﻿using Avalonia.Styling;
+using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace PasswordManager.UI.ViewModels;
 
 public partial class SettingsViewModel : ViewModelBase
 {
+    [ObservableProperty]
+    private bool _isDarkTheme = true;
 
+    partial void OnIsDarkThemeChanged(bool value)
+    {
+        if (Avalonia.Application.Current != null)
+            Avalonia.Application.Current.RequestedThemeVariant = value ? ThemeVariant.Dark : ThemeVariant.Light;
+    }
 }
